e0b9a3f2995e7a060002951eecf96cc1ad0dd9c2
Nabil <nabilaltamash@gmail.com>
2021-07-29 12:49:20 +0600

    Initial commit - Nabil

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+// Food item 
+struct MenuItem {
+  int serial;
+  int price; 
+  char name[20];
+};
+
+// Order struct 
+struct Order {
+  int menuItemSerial; 
+  int quantity; 
+  struct Order *next; 
+} *front, *rear, *top; 
+
+// Helper functions 
+void enqueue(); 
+void push(); 
+void dequeue(); 
+
+
+// Place an order 
+void placeOrder(); 
+// Serve an order 
+void serve(); 
+// Show receipt 
+void showReceipt(); 
+// Show initial menu 
+void showUI(); 
+// Show food menu 
+void showMenu(); 
+// Show both served and waiting 
+void showAll(); 
+// Show sold items 
+void showSold(); 
+// Show waiting queue 
+void showQueue(); 
+
+
+int main()
+{
+
+
+
+  return 0;
+}
