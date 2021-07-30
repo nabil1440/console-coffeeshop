64471de64f367fb06265f85448b299beb30f6230
Ihsan-nahid <nahidboss96@gmail.com>
2021-07-30 16:17:06 +0600

    function for showing stack and queue

--- main.cpp
+++ main.cpp
@@ -32,10 +32,13 @@ void showReceipt();
 void showUI(); 
 // Show food menu 
 void showMenu(); 
+
 // Show both served and waiting 
 void showAll(); 
+
 // Show sold items 
 void showSold(); 
+
 // Show waiting queue 
 void showQueue(); 
 
@@ -46,4 +49,38 @@ int main()
   MenuItem menu[5] = {};
 
   return 0;
+}
+
+void showAll()
+{
+  cout << "List of Items Are Sold" << endl;
+  showSold();
+  cout << endl;
+  cout << "List of Items Are is the Serial" << endl; 
+  showQueue();
+  cout << endl;
+}
+
+//show stack
+void showSold()
+{
+  Order *temp = top;
+  while(temp != NULL)
+  {
+    cout << temp->menuItemSerial << endl;
+    cout << temp->quantity << endl;
+    temp = temp->next;
+  }
+}
+
+//show the queue
+void showQueue()
+{
+  Order *temp = front;
+  while(temp != *rear)
+  {
+    cout << temp->menuItemSerial << endl;
+    cout << temp->quantity << endl;
+    temp = temp->next;
+  }
 }
